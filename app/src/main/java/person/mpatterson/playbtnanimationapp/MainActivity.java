96e25d7b56f23a855ab30c3fc786eb367962fa23
Michael <mpatterson@doubleencore.com>
2016-04-23 17:50:28 -0600

    start a trim animation on the circle onClick

--- app/src/main/java/person/mpatterson/playbtnanimationapp/MainActivity.java
+++ app/src/main/java/person/mpatterson/playbtnanimationapp/MainActivity.java
@@ -1,13 +1,32 @@
 package person.mpatterson.playbtnanimationapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.play_button)
+    protected ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.play_button)
+    protected void startAnimation() {
+        Drawable drawable = mImageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
     }
 }
